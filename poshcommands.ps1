d5310d4913823bd5812760c522a0c5fbc2199e3d
Grant Fritchey <grant@scarydba.com>
2025-01-07 10:02:32 -0600

    Adding the buildspec.yml file for Developer Tools

--- poshcommands.ps1
+++ poshcommands.ps1
@@ -1,8 +1,23 @@
 ## core command to run Flyway
+## assumes a configuration file is defined
 flyway migrate
 
-## assumes a configuration file is defined
+## get a full understanding of the migrations available
+flyway info
+
+## not what it sounds like
+flyway clean
 
 
 
+## supplying the connection information
+flyway validate -url=jdbc:postgresql://hsr.c2ek9ilzyxhz.us-east-1.rds.amazonaws.com:5432/hamshackradio -user=postgres -password=*cthulhu1988
+
+flyway migrate  -url=jdbc:postgresql://hsr.c2ek9ilzyxhz.us-east-1.rds.amazonaws.com:5432/hamshackradio -user=postgres -password=*cthulhu1988
+
+
+## necessary after cleaning out test deployments
 flyway repair
+
+
+flyway baseline
